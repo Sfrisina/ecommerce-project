215a9a035743a01228de35cabb957a1ae6ff001b
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2022-09-27 17:14:25 -0400

    lots of changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import{Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import{ Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-    <Header />
-    <main className='py- 3'>
+    <Router>
+      <Header />
+    <main className='py-3'>
       <Container>
-                 <h1>Welcome</h1>
+        <Routes>
+                <Route path='/' element={ <HomeScreen />}></Route>
+                <Route path='/product/:id' element={<ProductScreen />}></Route>
+        </Routes>
       </Container>
     </main>
     <Footer />
-    </>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
