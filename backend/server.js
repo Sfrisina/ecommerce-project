d701149b694b8ea64663d6ae6bc005c0c8adfe11
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2022-10-17 15:21:20 -0400

    initial deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
@@ -23,11 +24,12 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+const __dirname = path.resolve()
 
-if(process.env.NODE.ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend, build, index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }else{
     
 app.get('/', (req, res) => {
